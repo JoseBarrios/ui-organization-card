6e6ee7c57e992b097182af4300b729339054267c
Jose Barrios <jose@cognilab.com>
2017-07-22 02:05:24 -0700

    Build

--- element/data-controller.js
+++ element/data-controller.js
@@ -68,144 +68,202 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 //WEBPACK: Makes it browser friendly
-window.JobPosting = __webpack_require__(1)
+window.Organization = __webpack_require__(1)
 
 
 /***/ }),
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-const Intangible = __webpack_require__(2)
+const Thing = __webpack_require__(2);
+const TYPE = 'Organization'
+const EMPTY = '';
 
-class JobPosting extends Intangible {
+class Organization extends Thing {
+
+  static get type(){ return TYPE; }
 
   constructor(model){
     model = model || {};
-    super(model)
+    super(model);
 
-    this.baseSalary = model.baseSalary;
-    this.datePosted = model.datePosted;
-    this.educationRequirements = model.educationRequirements;
-    this.employmentType = model.employmentType;
-    this.experienceRequirements = model.experienceRequirements;
-    this.hiringOrganization = model.hiringOrganization;
-    this.incentiveCompensation = model.incentiveCompensation;
-    this.industry = model.industry;
-    this.jobBenefits = model.jobBenefits;
-    this.jobLocation = model.jobLocation;
-    this.occupationalCategory = model.occupationalCategory;
-    this.qualifications = model.qualifications;
-    this.responsibilities = model.responsibilities;
-    this.salaryCurrency = model.salaryCurrency;
-    this.skills = model.skills;
-    this.specialCommitments = model.specialCommitments;
-    this.title = model.title;
-    this.validThrough = model.validThrough;
-    this.workHours = model.workHours;
+    this.address = model.address;
+    this.aggregateRating = model.aggregateRating
+    this.alumni = model.alumni;
+    this.areaServed = model.areaServed;
+    this.award = model.award;
+    this.brand = model.brand;
+    this.contactPoint = model.contactPoint;
+    this.department = model.department;
+    this.dissolutionDate = model.dissolutionDate;
+    this.duns = model.duns;
+    this.email = model.email;
+    this.employee = model.employee;
+    this.event = model.event;
+    this.faxNumber = model.faxNumber;
+    this.founder = model.founder;
+    this.foundingDate = model.foundingDate;
+    this.foundingLocation = model.foundingLocation;
+    this.funder = model.funder;
+    this.globalLocationNumber = model.globalLocationNumber;
+    this.hasOfferCatalog = model.hasOfferCatalog;
+    this.hasPOS = model.hasPOS;
+    this.isicV4 = model.isicV4;
+    this.legalName = model.legalName;
+    this.leiCode = model.leiCode;
+    this.location = model.location;
+    this.logo = model.logo;
+    this.makesOffer = model.makesOffer;
+    this.member = model.member;
+    this.memberOf = model.memberOf;
+    this.naics = model.naics;
+    this.numberOfEmployees = model.numberOfEmployees;
+    this.owns = model.owns;
+    this.parentOrganization = model.parentOrganization;
+    this.review = model.review;
+    this.seeks = model.seeks;
+    this.sponsor = model.sponsor;
+    this.subOrganization = model.subOrganization;
+    this.taxID = model.taxID;
+    this.telephone = model.telephone;
+    this.vatID = model.vatID;
   }
 
-  get baseSalary(){ return this.computed.baseSalary; }
-  set baseSalary(value){ this.computed.baseSalary = value; }
+  get address(){ return this.computed.address; }
+  set address(value){ this.computed.address = value; }
 
-  get datePosted(){ return this.computed.datePosted; }
-  set datePosted(value){ this.computed.datePosted = value; }
+  get aggregateRating(){ return this.computed.aggregateRating; }
+  set aggregateRating(value){ this.computed.aggregateRating = value; }
 
-  get educationRequirements(){ return this.computed.educationRequirements; }
-  set educationRequirements(value){
-    if(!this.computed.educationRequirements){ this.computed.educationRequirements = []; }
-    if(Intangible.isArray(value)){ this.computed.educationRequirements = value; }
-    else if(Intangible.isString(value)){ this.computed.educationRequirements = [value]; }
-  }
 
-  get employmentType(){ return this.computed.employmentType; }
-  set employmentType(value){ this.computed.employmentType = value; }
+  get alumni(){ return this.computed.alumni; }
+  set alumni(value){ this.computed.alumni = value; }
 
-  get experienceRequirements(){ return this.computed.experienceRequirements; }
-  set experienceRequirements(value){
-    if(Intangible.isArray(value)){ this.computed.experienceRequirements = value; }
-    else if(Intangible.isString(value)) { this.computed.experienceRequirements = [value]; }
-    else if(!this.computed.experienceRequirements){ this.computed.experienceRequirements = []; }
-  }
+  get areaServed(){ return this.computed.areaServed; }
+  set areaServed(value){ this.computed.areaServed = value; }
 
-  get hiringOrganization(){ return this.computed.hiringOrganization; }
-  set hiringOrganization(value){ this.computed.hiringOrganization = value; }
+  get award(){ return this.computed.award; }
+  set award(value){ this.computed.award = value; }
 
-  get incentiveCompensation(){ return this.computed.incentiveCompensation; }
-  set incentiveCompensation(value){ this.computed.incentiveCompensation = value; }
 
-  get industry(){ return this.computed.industry; }
-  set industry(value){ this.computed.industry = value; }
+  get brand(){ return this.computed.brand; }
+  set brand(value){ this.computed.brand = value; }
 
-  get jobBenefits(){ return this.computed.jobBenefits; }
-  set jobBenefits(value){ this.computed.jobBenefits = value; }
+  get contactPoint(){ return this.computed.contactPoint; }
+  set contactPoint(value){ this.computed.contactPoint = value; }
 
-  get jobLocation(){ return this.computed.jobLocation; }
-  set jobLocation(value){ this.computed.jobLocation = value; }
+  get department(){ return this.computed.department; }
+  set department(value){ this.computed.department = value; }
 
-  get occupationalCategory(){ return this.computed.occupationalCategory; }
-  set occupationalCategory(value){ this.computed.occupationalCategory = value; }
+  get dissolutionDate(){ return this.computed.dissolutionDate; }
+  set dissolutionDate(value){ this.computed.dissolutionDate = value; }
 
-  get qualifications(){ return this.computed.qualifications; }
-  set qualifications(value){
-    if(!this.computed.qualifications){ this.computed.qualifications = []; }
-    if(Intangible.isArray(value)){ this.computed.qualifications = value; }
-    else if(Intangible.isString(value)) { this.computed.qualifications = [value]; }
-  }
+  get duns(){ return this.computed.duns; }
+  set duns(value){ this.computed.duns = value; }
 
-  get responsibilities(){ return this.computed.responsibilities; }
-  set responsibilities(value){
-    if(!this.computed.responsibilities){ this.computed.responsibilities = []; }
-    if(Intangible.isArray(value)){ this.computed.responsibilities = value; }
-    else if(Intangible.isString(value)) { this.computed.responsibilities = [value]; }
-  }
+  get email(){ return this.computed.email; }
+  set email(value){ this.computed.email = value; }
 
-  get salaryCurrency(){ return this.computed.salaryCurrency; }
-  set salaryCurrency(value){ this.computed.salaryCurrency = value; }
+  get employee(){ return this.computed.employee; }
+  set employee(value){ this.computed.employee = value; }
 
-  get skills(){ return this.computed.skills; }
-  set skills(value){ this.computed.skills = value; }
+  get event(){ return this.computed.event; }
+  set event(value){ this.computed.event = value; }
 
-  get specialCommitments(){ return this.computed.specialCommitments; }
-  set specialCommitments(value){ this.computed.specialCommitments = value; }
+  get faxNumber(){ return this.computed.faxNumber; }
+  set faxNumber(value){ this.computed.faxNumber = value; }
 
-  get title(){ return this.computed.title; }
-  set title(value){ this.computed.title = value; }
+  get founder(){ return this.computed.founder; }
+  set founder(value){ this.computed.founder = value; }
 
-  get validThrough(){ return this.computed.validThrough; }
-  set validThrough(value){ this.computed.validThrough = value; }
+  get foundingDate(){ return this.computed.foundingDate; }
+  set foundingDate(value){ this.computed.foundingDate = value; }
 
-  get workHours(){ return this.computed.workHours; }
-  set workHours(value){ this.computed.workHours = value; }
+  get foundingLocation(){ return this.computed.foundingLocation; }
+  set foundingLocation(value){ this.computed.foundingLocation = value; }
+
+  get funder(){ return this.computed.funder; }
+  set funder(value){ this.computed.funder = value; }
+
+  get globalLocationNumber(){ return this.computed.globalLocationNumber; }
+  set globalLocationNumber(value){ this.computed.globalLocationNumber = value; }
+
+  get hasOfferCatalog(){ return this.computed.hasOfferCatalog; }
+  set hasOfferCatalog(value){ this.computed.hasOfferCatalog = value; }
+
+  get hasPOS(){ return this.computed.hasPOS; }
+  set hasPOS(value){ this.computed.hasPOS = value; }
+
+  get isicV4(){ return this.computed.isicV4; }
+  set isicV4(value){ this.computed.isicV4 = value; }
+
+  get legalName(){ return this.computed.legalName; }
+  set legalName(value){ this.computed.legalName = value; }
+
+  get leiCode(){ return this.computed.leiCode; }
+  set leiCode(value){ this.computed.leiCode = value; }
+
+  get location(){ return this.computed.location; }
+  set location(value){ this.computed.location = value; }
+
+  get logo(){ return this.computed.logo; }
+  set logo(value){ this.computed.logo = value; }
+
+  get makesOffer(){ return this.computed.makesOffer; }
+  set makesOffer(value){ this.computed.makesOffer = value; }
+
+  get member(){ return this.computed.member; }
+  set member(value){ this.computed.member = value; }
+
+  get memberOf(){ return this.computed.memberOf; }
+  set memberOf(value){ this.computed.memberOf = value; }
+
+  get naics(){ return this.computed.naics; }
+  set naics(value){ this.computed.naics = value; }
+
+  get numberOfEmployees(){ return this.computed.numberOfEmployees; }
+  set numberOfEmployees(value){ this.computed.numberOfEmployees = value; }
+
+  get owns(){ return this.computed.owns; }
+  set owns(value){ this.computed.owns = value; }
+
+  get parentOrganization(){ return this.computed.parentOrganization; }
+  set parentOrganization(value){ this.computed.parentOrganization = value; }
+
+  get review(){ return this.computed.review; }
+  set review(value){ this.computed.review = value; }
+
+  get seeks(){ return this.computed.seeks; }
+  set seeks(value){ this.computed.seeks = value; }
+
+  get sponsor(){ return this.computed.sponsor; }
+  set sponsor(value){ this.computed.sponsor = value; }
+
+  get subOrganization(){ return this.computed.subOrganization; }
+  set subOrganization(value){ this.computed.subOrganization = value; }
+
+  get taxID(){ return this.computed.taxID; }
+  set taxID(value){ this.computed.taxID = value; }
+
+  get telephone(){ return this.computed.telephone; }
+  set telephone(value){ this.computed.telephone = value; }
+
+  get vatID(){ return this.computed.vatID; }
+  set vatID(value){ this.computed.vatID = value; }
+
 }
 
-module.exports = JobPosting;
+module.exports = Organization;
 
 
 /***/ }),
 /* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
-const Thing = __webpack_require__(3);
-
-class Intangible extends Thing{
-
-  constructor(model){
-    model = model || {};
-    super(model)
-  }
-}
-
-module.exports = Intangible;
-
-
-/***/ }),
-/* 3 */
-/***/ (function(module, exports, __webpack_require__) {
-
 "use strict";
 
 
-const lodash = __webpack_require__(4);
+const lodash = __webpack_require__(3);
 const EMPTY = '';
 const TYPE = 'Thing';
 let muteErrors = false;
@@ -498,7 +556,7 @@ module.exports = Thing;
 
 
 /***/ }),
-/* 4 */
+/* 3 */
 /***/ (function(module, exports, __webpack_require__) {
 
 /* WEBPACK VAR INJECTION */(function(global, module) {var __WEBPACK_AMD_DEFINE_RESULT__;/**
@@ -17587,10 +17645,10 @@ module.exports = Thing;
   }
 }.call(this));
 
-/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(5), __webpack_require__(6)(module)))
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(4), __webpack_require__(5)(module)))
 
 /***/ }),
-/* 5 */
+/* 4 */
 /***/ (function(module, exports) {
 
 var g;
@@ -17617,7 +17675,7 @@ module.exports = g;
 
 
 /***/ }),
-/* 6 */
+/* 5 */
 /***/ (function(module, exports) {
 
 module.exports = function(module) {
