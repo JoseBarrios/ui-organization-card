45a397fd195786ef62f2b1eff1dfb7e6506af819
Jose Barrios <jose@cognilab.com>
2017-07-29 23:03:38 -0700

    Added element API

--- element/view-controller.js
+++ element/view-controller.js
@@ -4,16 +4,32 @@ let template = uiOrganizationCardDoc.ownerDocument.querySelector('#ui-organizati
 
 class OrganizationCardViewController extends HTMLElement {
 
+  static get observedAttributes(){
+    return ['model'];
+  }
+
   constructor(){
     super();
+    this.model = new Organization();
+    const view = document.importNode(template.content, true);
+    this.shadowRoot = this.attachShadow({mode: 'open'});
+    this.shadowRoot.appendChild(view);
   }
 
 	///STANDARD
 	connectedCallback() {
-    console.log('connected');
-    const view = document.importNode(template.content, true);
-    let shadowRoot = this.attachShadow({mode: 'open'});
-    shadowRoot.appendChild(view);
+    console.log('connected', this);
+    this.$container = this.shadowRoot.querySelector('.container');
+    this.$image = this.shadowRoot.querySelector('#image');
+    this.$name = this.shadowRoot.querySelector('#name');
+    this.$disamiguatingDescription = this.shadowRoot.querySelector('#disamiguatingDescription');
+    this.$location = this.shadowRoot.querySelector('#location');
+    this.$description = this.shadowRoot.querySelector('#description');
+    this._updateRendering();
+	}
+
+	adoptedCallback(){
+    console.log('adoptedCallback');
 	}
 
 	disconnectedCallback() {
@@ -21,12 +37,23 @@ class OrganizationCardViewController extends HTMLElement {
 	}
 
 	attributeChangedCallback(attrName, oldVal, newVal) {
-    console.log('attributeChanged');
-	}
+    let model = JSON.parse(newVal);
+    this.model = new Organization(model);
+    console.log(this.model)
+    this._updateRendering();
+  }
 
-	adoptedCallback(){
-    console.log('adoptedCallback');
-	}
+  get shadowRoot(){return this._shadowRoot;}
+  set shadowRoot(value){ this._shadowRoot = value}
+
+  _updateRendering() {
+    if(this.$image){ this.$image.src = this.model.image; }
+    if(this.$name){ this.$name.textContent = this.model.name; }
+    if(this.$disambiguatingDescription){ this.$disambiguatingDescription.textContent = this.model.disambiguatingDescription; }
+    if(this.$location){ this.$location.textContent = this.model.location; }
+    if(this.$description){ this.$description.textContent = this.model.description; }
+  }
+
 }
 
 window.customElements.define('ui-organization-card', OrganizationCardViewController);
